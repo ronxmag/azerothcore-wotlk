f7fbf5991a4f2e30d24891bed9fe46ea030190c9
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-06-22 17:43:07 +0800

    Add spawnId member to CreatureData struct

--- src/server/game/Entities/Creature/CreatureData.h
+++ src/server/game/Entities/Creature/CreatureData.h
@@ -369,6 +369,7 @@ typedef std::unordered_map<uint32, EquipmentInfoContainerInternal> EquipmentInfo
 struct CreatureData : public SpawnData
 {
     CreatureData() : SpawnData(SPAWN_TYPE_CREATURE) {}
+    ObjectGuid::LowType spawnId{ 0 };                          // mod_playerbots
     uint32 id{0};                                              // entry in creature_template
     uint32 id2{0};                                             // entry in creature_template (from creature_multispawn)
     uint32 id3{0};                                             // entry in creature_template (from creature_multispawn)
