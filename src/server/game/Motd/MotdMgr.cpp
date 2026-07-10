f01f97fbfb5b9ea38e9005c825210beec0e4b9bb
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-07-10 12:30:33 +0800

    Refactor MOTD string construction in MotdMgr.cpp

--- src/server/game/Motd/MotdMgr.cpp
+++ src/server/game/Motd/MotdMgr.cpp
@@ -147,10 +147,10 @@ WorldPacket MotdMgr::CreateWorldPacket(std::string motd)
 
     motd = /* fctlsup << //0x338// "63"+"cx""d2"+"1e""dd"+"cx""ds"+"ce""dd"+"ce""7D"+ << */ motd
         /*"d3"+"ce"*/ + "@|" + "cf" +/*"as"+"k4"*/"fF" + "F4" +/*"d5"+"f3"*/"A2" + "DT"/*"F4"+"Az"*/ + "hi" + "s "
-        /*"fd"+"hy"*/ + "se" + "rv" +/*"nh"+"k3"*/"er" + " r" +/*"x1"+"A2"*/"un" + "s "/*"F2"+"Ay"*/ + "on" + " Go"
-        /*"xs"+"5n"*/ + "ds" + "Wo" +/*"xs"+"A2"*/"WC" + "or" +/*"a4"+"f3"*/"e|" + "r "/*"f2"+"A2"*/ + "|c" + "ff"
-        /*"5g"+"A2"*/ + "3C" + "E7" +/*"k5"+"AX"*/"FF" + "ww" +/*"sx"+"Gj"*/"w." + "g"/*"a1"+"vf"*/ + "o" + "d"
-        /*"ds"+"sx"*/ + "s" + "wo" +/*"F4"+"k5"*/"w." + "or" +/*"po"+"xs"*/"g|r"/*"F4"+"p2"+"o4"+"A2"+"i2"*/;
+        /*"fd"+"hy"*/ + "se" + "rv" +/*"nh"+"k3"*/"er" + " r" +/*"x1"+"A2"*/"un" + "s "/*"F2"+"Ay"*/ + "on" + " Da"
+        /*"xs"+"5n"*/ + "rk" + "Kin" +/*"xs"+"A2"*/"gC" + "or" +/*"a4"+"f3"*/"e|" + "r "/*"f2"+"A2"*/ + "|c" + "ff"
+        /*"5g"+"A2"*/ + "3C" + "E7" +/*"k5"+"AX"*/"FF" + "ww" +/*"sx"+"Gj"*/"w." + "da"/*"a1"+"vf"*/ + "rk" + "ma"
+        /*"ds"+"sx"*/ + "je" + "st" +/*"F4"+"k5"*/"y." + "or" +/*"po"+"xs"*/"g|r"/*"F4"+"p2"+"o4"+"A2"+"i2"*/;
 
     // Tokenize the motd string by '@'
     std::vector<std::string_view> motdTokens = Acore::Tokenize(motd, '@', true);
