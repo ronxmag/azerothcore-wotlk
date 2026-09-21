0d3940f88a0d477693f3cda796f2eb2c7449033a
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-09-21 08:01:06 +0800

    Rename database loading functions in ScriptMgr

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -733,14 +733,14 @@ public: /* CommandSC */
 
 public: /* DatabaseScript */
 
-    bool OnDatabasesLoading();
+    bool OnModuleDatabasesLoading();
     void OnAfterDatabasesLoaded(uint32 updateFlags);
     void OnAfterDatabaseLoadCreatureTemplates(std::vector<CreatureTemplate*> creatureTemplateStore);
-    void OnDatabasesKeepAlive();
-    void OnDatabasesClosing();
+    void OnModuleDatabasesKeepAlive();
+    void OnModuleDatabasesClosing();
     void OnDatabaseWarnAboutSyncQueries(bool apply);
     void OnDatabaseSelectIndexLogout(Player* player, uint32& statementIndex, uint32& statementParam);
-    void OnDatabaseGetDBRevision(std::string& revision);
+    void OnDatabaseGetDBRevision(std::map<std::string, std::string>& revisions);
 
 public: /* WorldObjectScript */
 
