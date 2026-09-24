f4f3cdea1e5b7e11b513355813a0ae091dab2af0
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-09-25 07:08:07 +0800

    Improve movement generation logic in PointMovementGenerator
    Refactor movement initialization and path handling.

--- src/server/game/Movement/MovementGenerators/PointMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/PointMovementGenerator.cpp
@@ -53,7 +53,6 @@ void PointMovementGenerator<T>::DoInitialize(T* unit)
     i_recalculateSpeed = false;
     Movement::MoveSplineInit init(unit);
 
-    // mod-playerbots
     if (_reverseOrientation)
         init.SetOrientationInversed();
 
@@ -152,6 +151,10 @@ bool PointMovementGenerator<T>::DoUpdate(T* unit, uint32 diff)
     {
         i_recalculateSpeed = false;
         Movement::MoveSplineInit init(unit);
+
+        if (_reverseOrientation)
+            init.SetOrientationInversed();
+
         auto rebasePrecomputedPath = [this, unit](std::optional<uint32> offset = std::nullopt)
         {
             Movement::PointsArray rebasedPath;
