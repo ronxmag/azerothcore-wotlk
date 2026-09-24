5bce168f270c77511e4c227dc2c291577521b0a7
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-09-25 06:55:53 +0800

    Refactor LFG proposal checks in LFGQueue.cpp

--- src/server/game/DungeonFinding/LFGQueue.cpp
+++ src/server/game/DungeonFinding/LFGQueue.cpp
@@ -412,10 +412,8 @@ namespace lfg
         if (!sLFGMgr->AllQueued(check)) // can't create proposal
             return LFG_COMPATIBILITY_PENDING;
 
-        if (!sScriptMgr->OnPlayerbotCheckLFGQueue(proposal.queues))
-        {
-            return LFG_INCOMPATIBLES_HAS_IGNORES;
-        }
+        if (!sScriptMgr->CanCreateLfgProposal(proposal.queues))
+            return LFG_INCOMPATIBLES_REJECTED_BY_SCRIPT;
 
         // Create a new proposal
         proposal.cancelTime = GameTime::GetGameTime().count() + LFG_TIME_PROPOSAL;
