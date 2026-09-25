5a6b6ce1491268c55157350ed4f4ede3a6dd66f0
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-09-25 10:14:33 +0800

    Remove QueryHolder.h and update AddQueryHolderCallback
    Removed unused QueryHolder.h include and adjusted method signature for AddQueryHolderCallback.

--- src/server/game/World/IWorld.h
+++ src/server/game/World/IWorld.h
@@ -22,7 +22,6 @@
 #include "Common.h"
 #include "Duration.h"
 #include "ObjectGuid.h"
-#include "QueryHolder.h"
 #include "SharedDefines.h"
 #include "WorldConfig.h"
 #include <unordered_map>
@@ -112,7 +111,6 @@ public:
     [[nodiscard]] virtual std::string const& GetRealmName() const = 0;
     virtual void SetRealmName(std::string name) = 0;
     virtual void ReloadRBAC() = 0;
-    virtual SQLQueryHolderCallback& AddQueryHolderCallback(SQLQueryHolderCallback&& callback) = 0;
 };
 
 #endif //AZEROTHCORE_IWORLD_H
