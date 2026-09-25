4689ede1f993cb94cc650128b2087ac463be536f
TagaBukiDnaSosyaL <55305997+ronxmag@users.noreply.github.com>
2026-09-25 10:18:07 +0800

    Remove AddQueryHolderCallback and AsyncCallbackProcessor
    Removed AddQueryHolderCallback method and related AsyncCallbackProcessor.

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -258,9 +258,6 @@ protected:
     void ResetRandomBG();
     void CalendarDeleteOldEvents();
     void ResetGuildCap();
-
-    SQLQueryHolderCallback& AddQueryHolderCallback(SQLQueryHolderCallback&& callback) override;
-
 private:
     WorldConfig _worldConfig;
 
@@ -308,7 +305,6 @@ private:
 
     void ProcessQueryCallbacks();
     QueryCallbackProcessor _queryProcessor;
-    AsyncCallbackProcessor<SQLQueryHolderCallback> _queryHolderProcessor;
 
     /**
      * @brief Executed when a World Session is being finalized. Be it from a normal login or via queue popping.
